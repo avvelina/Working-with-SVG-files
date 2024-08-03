77fce81808ffd1edaea36f33c01f20b563c083d1
Alexandra Velina <44321182+avvelina@users.noreply.github.com>
2024-08-03 22:25:51 +0300

    Update Rectangle.h

--- Rectangle.h
+++ Rectangle.h
@@ -1,6 +1,4 @@
-/*SVG Project created by Alexandra Velina
- *		   Informarics, 2020
- */
+/*SVG Project created by Alexandra Velina, 2020 */
 
 #pragma once
 #ifndef RECTANGLE_H
@@ -15,73 +13,48 @@ class Rectangle : public Shape
 {
 public:
 
-	/**
-	 * Constructor with parameters.
-	 */
+	/* Constructor with parameters. */
 	Rectangle(double xC = 0, double yC = 0, double w = 0, double h = 0, const char* f = "none");
 
-	/**
-	 * Copy constructor.
-	 */
+	/* Copy constructor. */
 	Rectangle(const Rectangle& rect);
 
-	/**
-	 * Redefine an operator.
-	 */
+	/* Redefine an operator. */
 	Rectangle& operator= (const Rectangle& rect);
 
-	/**
-	 * Virtual destructor.
-	 */
+	/* Virtual destructor. */
 	virtual ~Rectangle();
 
-	/**
-	 * Get Function Declarations for private variables
-	 */
+	/* Get Function Declarations for private variables. */
 	double getX()         const;
 	double getY()         const;
 	double getWidth()     const;
 	double getHeight()    const;
 	const char* getFill() const;
 
-	/**
-	 * Set Function Declarations for private variables
-	 */
+	/* Set Function Declarations for private variables. */
 	void setX(double x);
 	void setY(double y);
 	void setWidth(double width);
 	void setHeight(double height);
 	void setFill(const char* f);
 
-	/**
-	 * An output operator that prints the circle.
-	 */
+	/* An output operator that prints the circle. */
 	virtual ostream& Print(ostream& out) const;
 
-	/**
-	 * Enters the information in the file.
-	 */
+	/* Enters the information in the file. */
 	virtual void WriteFile(ofstream& of) const;
 
-	/**
-	 * A virtual function from Shape that returns a pointer to Shape,
-	 * where a new object with the same data is created.
-	 */
+	/* A virtual function from Shape that returns a pointer to Shape, where a new object with the same data is created. */
 	virtual Shape* Clone() const;
 
-	/**
-	 * Translates the circle.
-	 */
+	/* Translates the circle. */
 	virtual void Translate(int v, int h);
 
-	/**
-	 * Virtual function checks if the rectangle contains a cirlce.
-	 */
+	/* Virtual function checks if the rectangle contains a cirlce. */
 	virtual bool IsWithinCircle(double cx, double cy, double r) const;
 
-	/**
-	 * Virtual function checks if the rectangle contains a rectangle.
-	 */
+	/* Virtual function checks if the rectangle contains a rectangle. */
 	virtual bool IsWithinRectangle(double x, double y, double width, double height) const;
 
 private:
