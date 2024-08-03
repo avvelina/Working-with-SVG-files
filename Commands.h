901f3888e0381e10d2268caa5768c13e5dcd176e
Alexandra Velina <44321182+avvelina@users.noreply.github.com>
2024-08-03 22:20:31 +0300

    Update Commands.h

--- Commands.h
+++ Commands.h
@@ -1,20 +1,16 @@
-/*SVG Project created by Alexandra Velina
- *		   Informarics, 2020
- */
+/*SVG Project created by Alexandra Velina, 2020 */
 
 #pragma once
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
- /**
-  * This class represents the file operations that should be executed on an SVG file.
+ /* This class represents the file operations that should be executed on an SVG file.
   * - prints the menu with commands on the console
   * - open a file
   * - save the opened file
   * - save the SVG shapes as a new file
   * - displays brief information about the commands
-  * - closes the program
-  */
+  * - closes the program */
 class Commands
 {
 public:
